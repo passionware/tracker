baa129209997d2bea286aeee2d5a97a387dc9c1d
Adam Borowski <fl.borovsky@gmail.com>
2025-01-06 23:13:26 +0100

    fix: ts

--- src/features/app/LoginWidget.tsx
+++ src/features/app/LoginWidget.tsx
@@ -1,8 +1,14 @@
 import { LoginForm } from "@/features/app/login-form.tsx";
 import { WithServices } from "@/platform/typescript/services.ts";
+import { WithRoutingService } from "@/services/front/RoutingService/RoutingService.ts";
+import { WithNavigationService } from "@/services/internal/NavigationService/NavigationService.ts";
 import { WithAuthService } from "@/services/io/AuthService/AuthService.ts";
 
-export function LoginPage(props: WithServices<[WithAuthService]>) {
+export function LoginPage(
+  props: WithServices<
+    [WithAuthService, WithRoutingService, WithNavigationService]
+  >,
+) {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-slate-100 p-6 md:p-10 dark:bg-slate-800">
       <div className="w-full max-w-sm md:max-w-3xl">
