43b890bf522861882596af1149d37cc8cfe8ed02
Adam Borowski <fl.borovsky@gmail.com>
2025-01-19 10:36:33 +0100

    chore: upgrade core

--- src/services/io/VariableService/VariableService.mock.ts
+++ src/services/io/VariableService/VariableService.mock.ts
@@ -1,13 +1,58 @@
+import { Variable } from "@/api/variable/variable.api.ts";
 import { variableMock } from "@/api/variable/variable.mock.ts";
 import { VariableService } from "@/services/io/VariableService/VariableService.ts";
-import { rd } from "@passionware/monads";
+import { rd, RemoteData } from "@passionware/monads";
+import { ArgsAccessor } from "@passionware/platform-storybook";
 
-export function createVariableService(): VariableService {
+export function createVariableService(config: {
+  onAction?: (action: string, ...rest: unknown[]) => void;
+  data?:
+    | {
+        initial: Variable[];
+      }
+    | {
+        accessor: ArgsAccessor<{ variables: RemoteData<Variable[]> }>;
+      };
+}): VariableService {
+  function createUseVariables() {
+    const data = config.data;
+    if (!data) {
+      return () => rd.of(variableMock.static.list);
+    }
+    if ("initial" in data) {
+      return () => rd.of(data.initial);
+    } else {
+      return () => data.accessor.useArgs().variables;
+    }
+  }
+
+  function createEnsureVariables() {
+    const data = config.data;
+    if (!data) {
+      return () => Promise.resolve(variableMock.static.list);
+    }
+    if ("initial" in data) {
+      return () => Promise.resolve(data.initial);
+    } else {
+      return () =>
+        Promise.resolve(rd.getOrThrow(data.accessor.getLatestArgs().variables));
+    }
+  }
+
   return {
-    useVariables: () => rd.of(variableMock.static.list),
-    ensureVariables: () => Promise.resolve(variableMock.static.list),
-    createVariable: () => Promise.resolve({ id: 1 }),
-    deleteVariable: () => Promise.resolve(),
-    updateVariable: () => Promise.resolve(),
+    useVariables: createUseVariables(),
+    ensureVariables: createEnsureVariables(),
+    createVariable: (variable) => {
+      config.onAction?.("createVariable", variable);
+      return Promise.resolve({ id: 1 });
+    },
+    deleteVariable: (id) => {
+      config.onAction?.("deleteVariable", id);
+      return Promise.resolve();
+    },
+    updateVariable: (id, variable) => {
+      config.onAction?.("updateVariable", { id, variable });
+      return Promise.resolve();
+    },
   };
 }
