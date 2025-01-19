e82e6d63ca7f195c0c705c38ca501cd8055b7d5f
Adam Borowski <fl.borovsky@gmail.com>
2025-01-19 01:49:35 +0100

    feat: continuation

--- src/features/billing/BillingWidget.columns.tsx
+++ src/features/billing/BillingWidget.columns.tsx
@@ -92,7 +92,7 @@ export function useColumns(props: BillingWidgetProps) {
               }
             </Badge>
           </PopoverTrigger>
-          <PopoverContent className="w-fit">
+          <PopoverContent className="w-fit max-h-[calc(-1rem+var(--radix-popover-content-available-height))] overflow-y-auto">
             <PopoverHeader>Invoice details</PopoverHeader>
             <ChargeInfo services={props.services} billing={info.row.original} />
           </PopoverContent>
