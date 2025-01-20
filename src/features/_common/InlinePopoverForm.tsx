70096b655863f8313766751e28cfbc4e558d511d
Adam Borowski <fl.borovsky@gmail.com>
2025-01-20 19:18:03 +0100

    feat: styles

--- src/features/_common/InlinePopoverForm.tsx
+++ src/features/_common/InlinePopoverForm.tsx
@@ -17,9 +17,10 @@ export const InlinePopoverForm = ({ trigger, content }: InlinePopoverForm) => {
       {(bag) => (
         <Popover modal {...bag}>
           <PopoverTrigger asChild>{trigger}</PopoverTrigger>
-          {bag.open && <div className="fixed inset-0 bg-black/50 z-40" />}
+          {bag.open && <div className="fixed inset-0 backdrop-brightness-[0.7] backdrop-saturate-[0.3] z-40" />}
           <PopoverContent
             className="w-fit z-50 relative"
+            align="end"
             onEscapeKeyDown={bag.close}
             onInteractOutside={(event) => event.preventDefault()} // Prevent closing on outside click
           >
