d92bc28747501f1e059828e8eeecadcc2b0b6bac
Adam Borowski <fl.borovsky@gmail.com>
2025-01-25 12:15:00 +0100

    rafactor: structures

--- src/features/_common/elements/query/BillingQueryBar.tsx
+++ src/features/_common/elements/query/BillingQueryBar.tsx
@@ -1,14 +1,8 @@
-import { unassignedUtils } from "@/api/_common/query/filters/Unassigned.ts";
 import { BillingQuery, billingQueryUtils } from "@/api/billing/billing.api.ts";
 import { DateFilterWidget } from "@/features/_common/elements/filters/DateFilterWidget.tsx";
-import { ClientPicker } from "@/features/_common/elements/pickers/ClientPicker.tsx";
-import { ContractorMultiPicker } from "@/features/_common/elements/pickers/ContractorPicker.tsx";
-import { WorkspacePicker } from "@/features/_common/elements/pickers/WorkspacePicker.tsx";
+import { CommonQueryBar } from "@/features/_common/elements/query/_common.tsx";
 import { QueryBarLayout } from "@/features/_common/elements/query/QueryBarLayout.tsx";
-import {
-  QueryBarSpec,
-  queryBarSpecUtils,
-} from "@/features/_common/elements/query/QueryBarSpec.tsx";
+import { QueryBarSpec } from "@/features/_common/elements/query/QueryBarSpec.tsx";
 import { Nullable } from "@/platform/typescript/Nullable.ts";
 import { WithServices } from "@/platform/typescript/services.ts";
 import { WithFormatService } from "@/services/FormatService/FormatService.ts";
@@ -52,79 +46,12 @@ export function BillingQueryBar(props: BillingQueryBarProps) {
   }
   return (
     <QueryBarLayout>
-      {queryBarSpecUtils.renderIf(
-        props.spec.workspace,
-        <WorkspacePicker
-          size="sm"
-          allowClear
-          allowUnassigned
-          disabled={queryBarSpecUtils.isDisabled(props.spec.workspace)}
-          layout={
-            queryBarSpecUtils.isDisabled(props.spec.workspace)
-              ? "avatar"
-              : "full"
-          }
-          value={props.query.filters.workspaceId?.value[0]}
-          onSelect={handleChange("workspaceId", (workspaceId) =>
-            maybe.mapOrNull(
-              unassignedUtils.getOrElse(workspaceId, null),
-              (workspaceId) => ({
-                operator: "oneOf",
-                value: [workspaceId],
-              }),
-            ),
-          )}
-          services={props.services}
-        />,
-      )}
-      {queryBarSpecUtils.renderIf(
-        props.spec.client,
-        <ClientPicker
-          size="sm"
-          allowClear
-          allowUnassigned
-          disabled={queryBarSpecUtils.isDisabled(props.spec.client)}
-          layout={
-            queryBarSpecUtils.isDisabled(props.spec.client) ? "avatar" : "full"
-          }
-          services={props.services}
-          value={props.query.filters.clientId?.value[0]}
-          onSelect={handleChange("clientId", (clientId) =>
-            maybe.mapOrNull(
-              unassignedUtils.getOrElse(clientId, null),
-              (clientId) => ({
-                operator: "oneOf",
-                value: [clientId],
-              }),
-            ),
-          )}
-        />,
-      )}
-      {queryBarSpecUtils.renderIf(
-        props.spec.contractor,
-        <ContractorMultiPicker
-          size="sm"
-          allowUnassigned
-          services={props.services}
-          disabled={queryBarSpecUtils.isDisabled(props.spec.contractor)}
-          layout={
-            queryBarSpecUtils.isDisabled(props.spec.contractor)
-              ? "avatar"
-              : "full"
-          }
-          value={
-            props.query.filters.contractorId?.value.map(
-              unassignedUtils.fromMaybe,
-            ) ?? []
-          }
-          onSelect={handleChange("contractorId", (ids) =>
-            maybe.mapOrNull(maybe.fromArray(ids), (ids) => ({
-              operator: "oneOf",
-              value: ids.map(unassignedUtils.getOrNull),
-            })),
-          )}
-        />,
-      )}
+      <CommonQueryBar
+        query={props.query}
+        onQueryChange={props.onQueryChange}
+        spec={props.spec}
+        services={props.services}
+      />
       <DateFilterWidget
         services={props.services}
         value={props.query.filters.invoiceDate}
