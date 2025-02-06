c2ce549b0caa5b713ae8793968ad7dd4607a4358
Adam Borowski <fl.borovsky@gmail.com>
2025-02-06 17:24:41 +0100

    feat: projects widget

--- src/features/projects/ProjectListWidget.tsx
+++ src/features/projects/ProjectListWidget.tsx
@@ -1,15 +1,33 @@
+import { BreadcrumbPage } from "@/components/ui/breadcrumb.tsx";
+import { ClientBreadcrumbLink } from "@/features/_common/ClientBreadcrumbLink.tsx";
+import { CommonPageContainer } from "@/features/_common/CommonPageContainer.tsx";
+import { WorkspaceBreadcrumbLink } from "@/features/_common/WorkspaceBreadcrumbLink.tsx";
 import { WithServices } from "@/platform/typescript/services.ts";
 import {
   ClientSpec,
   WorkspaceSpec,
 } from "@/services/front/RoutingService/RoutingService.ts";
+import { WithClientService } from "@/services/io/ClientService/ClientService.ts";
+import { WithWorkspaceService } from "@/services/WorkspaceService/WorkspaceService.ts";
 
-export interface ProjectListWidgetProps extends WithServices<[]> {
+export interface ProjectListWidgetProps
+  extends WithServices<[WithWorkspaceService, WithClientService]> {
   filter: unknown; // something like all/current/past - should be part of ProjectQuery?
   clientId: ClientSpec;
   workspaceId: WorkspaceSpec;
 }
 
 export function ProjectListWidget(props: ProjectListWidgetProps) {
-  return <div>ProjectListWidget</div>;
+  return (
+    <CommonPageContainer
+      segments={[
+        <WorkspaceBreadcrumbLink {...props} />,
+        <ClientBreadcrumbLink {...props} />,
+        <BreadcrumbPage>Projects</BreadcrumbPage>,
+      ]}
+      tools={<></>}
+    >
+      Content
+    </CommonPageContainer>
+  );
 }
