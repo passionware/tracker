14116a501531ef45c87944384598c8d1c0a6c615
Adam Borowski <fl.borovsky@gmail.com>
2025-02-07 20:49:28 +0100

    feat: create, delete project api

--- src/features/projects/ProjectListWidget.tsx
+++ src/features/projects/ProjectListWidget.tsx
@@ -1,12 +1,17 @@
 import { projectQueryUtils } from "@/api/project/project.api.ts";
 import { BreadcrumbPage } from "@/components/ui/breadcrumb.tsx";
+import { Button } from "@/components/ui/button.tsx";
+import { PopoverHeader } from "@/components/ui/popover.tsx";
 import { ClientBreadcrumbLink } from "@/features/_common/ClientBreadcrumbLink.tsx";
 import { sharedColumns } from "@/features/_common/columns/_common/sharedColumns.tsx";
 import { project } from "@/features/_common/columns/project.tsx";
 import { CommonPageContainer } from "@/features/_common/CommonPageContainer.tsx";
 import { ProjectQueryBar } from "@/features/_common/elements/query/ProjectQueryBar.tsx";
+import { InlinePopoverForm } from "@/features/_common/InlinePopoverForm.tsx";
 import { ListView } from "@/features/_common/ListView.tsx";
+import { renderSmallError } from "@/features/_common/renderError.tsx";
 import { WorkspaceBreadcrumbLink } from "@/features/_common/WorkspaceBreadcrumbLink.tsx";
+import { ProjectForm } from "@/features/projects/_common/ProjectForm.tsx";
 import { idSpecUtils } from "@/platform/lang/IdSpec.ts";
 import { WithServices } from "@/platform/typescript/services.ts";
 import { WithFormatService } from "@/services/FormatService/FormatService.ts";
@@ -18,8 +23,12 @@ import {
 import { WithNavigationService } from "@/services/internal/NavigationService/NavigationService.ts";
 import { WithClientService } from "@/services/io/ClientService/ClientService.ts";
 import { WithContractorService } from "@/services/io/ContractorService/ContractorService.ts";
+import { WithMutationService } from "@/services/io/MutationService/MutationService.ts";
 import { WithProjectService } from "@/services/io/ProjectService/ProjectService.ts";
 import { WithWorkspaceService } from "@/services/WorkspaceService/WorkspaceService.ts";
+import { rd } from "@passionware/monads";
+import { promiseState } from "@passionware/platform-react";
+import { Check, Loader2, PlusCircle } from "lucide-react";
 import { useState } from "react";
 
 export interface ProjectListWidgetProps
@@ -32,6 +41,7 @@ export interface ProjectListWidgetProps
       WithContractorService,
       WithRoutingService,
       WithNavigationService,
+      WithMutationService,
     ]
   > {
   filter: unknown; // something like all/current/past - should be part of ProjectQuery?
@@ -43,6 +53,8 @@ export function ProjectListWidget(props: ProjectListWidgetProps) {
   const [_query, setQuery] = useState(projectQueryUtils.ofDefault());
   const query = projectQueryUtils.ensureDefault(_query, props);
   const projects = props.services.projectService.useProjects(query);
+
+  const addProjectState = promiseState.useRemoteData<void>();
 
   return (
     <CommonPageContainer
@@ -66,6 +78,45 @@ export function ProjectListWidget(props: ProjectListWidgetProps) {
               client: idSpecUtils.takeOrElse(props.clientId, "disable", "show"),
               contractor: "hide",
             }}
+          />
+          <InlinePopoverForm
+            trigger={
+              <Button variant="accent1" size="sm" className="flex">
+                {rd
+                  .fullJourney(addProjectState.state)
+                  .initially(<PlusCircle />)
+                  .wait(<Loader2 />)
+                  .catch(renderSmallError("w-6 h-6"))
+                  .map(() => (
+                    <Check />
+                  ))}
+                Add project
+              </Button>
+            }
+            content={(bag) => (
+              <>
+                <PopoverHeader>Add new project</PopoverHeader>
+                <ProjectForm
+                  onCancel={bag.close}
+                  defaultValues={{
+                    workspaceId: idSpecUtils.switchAll(
+                      props.workspaceId,
+                      undefined,
+                    ),
+                    clientId: idSpecUtils.switchAll(props.clientId, undefined),
+                    status: "draft",
+                  }}
+                  services={props.services}
+                  onSubmit={(data) =>
+                    addProjectState.track(
+                      props.services.mutationService
+                        .createProject(data)
+                        .then(bag.close),
+                    )
+                  }
+                />
+              </>
+            )}
           />
         </>
       }
