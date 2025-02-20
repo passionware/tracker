262c24605eb9e4ea23364d53461d4ac11e22f4fd
Adam Borowski <fl.borovsky@gmail.com>
2025-02-20 21:12:34 +0100

    refactor: cleanup

--- src/features/projects/iterations/ProjectIterationListWidget.tsx
+++ src/features/projects/iterations/ProjectIterationListWidget.tsx
@@ -3,14 +3,30 @@ import {
   projectIterationQueryUtils,
 } from "@/api/project-iteration/project-iteration.api.ts";
 import { Badge } from "@/components/ui/badge.tsx";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card.tsx";
+import { Skeleton } from "@/components/ui/skeleton.tsx";
 import { WithFrontServices } from "@/core/frontServices.ts";
+import {
+  ActionMenu,
+  ActionMenuDeleteItem,
+  ActionMenuEditItem,
+} from "@/features/_common/ActionMenu.tsx";
 import { sharedColumns } from "@/features/_common/columns/_common/sharedColumns.tsx";
+import { ClientWidget } from "@/features/_common/elements/pickers/ClientView.tsx";
+import { InlinePopoverForm } from "@/features/_common/InlinePopoverForm.tsx";
 import { ListView } from "@/features/_common/ListView.tsx";
+import { renderError } from "@/features/_common/renderError.tsx";
+import { ProjectForm } from "@/features/projects/_common/ProjectForm.tsx";
 import {
   ClientSpec,
   WorkspaceSpec,
 } from "@/services/front/RoutingService/RoutingService.ts";
-import { maybe } from "@passionware/monads";
+import { maybe, rd } from "@passionware/monads";
 import { createColumnHelper } from "@tanstack/react-table";
 import { capitalize } from "lodash";
 import { useState } from "react";
@@ -49,8 +65,95 @@ export function ProjectIterationListWidget(
   const projectIterations =
     props.services.projectIterationService.useProjectIterations(query);
 
+  const project = props.services.projectService.useProject(props.projectId);
+
   return (
     <>
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex flex-row gap-2 items-center">
+            <div className="text-slate-700 font-light">Project:</div>
+            <div>
+              {rd
+                .journey(project)
+                .wait(<Skeleton className="h-[1lh] w-30" />)
+                .catch(renderError)
+                .map((x) => x.name)}
+            </div>
+            <div>
+              {rd
+                .journey(project)
+                .wait(<Skeleton className="h-[1lh] w-30" />)
+                .catch(renderError)
+                .map((x) => (
+                  <ClientWidget
+                    size="sm"
+                    layout="avatar"
+                    clientId={x.clientId}
+                    services={props.services}
+                  />
+                ))}
+            </div>
+            <ActionMenu services={props.services} className="ml-auto">
+              {rd
+                .journey(project)
+                .wait(<Skeleton className="h-[1lh] w-30" />)
+                .catch(renderError)
+                .map((project) => (
+                  <>
+                    <ActionMenuDeleteItem
+                      onClick={async () => {
+                        await props.services.mutationService.deleteProject(
+                          project.id,
+                        );
+                        // navigate to the list
+                        props.services.navigationService.navigate(
+                          props.services.routingService
+                            .forWorkspace(props.workspaceId)
+                            .forClient(props.clientId)
+                            .projectsRoot(),
+                        );
+                      }}
+                    >
+                      Delete project
+                    </ActionMenuDeleteItem>
+                    <InlinePopoverForm
+                      trigger={
+                        <ActionMenuEditItem
+                          onSelect={(e) => e.preventDefault()}
+                        >
+                          Edit project
+                        </ActionMenuEditItem>
+                      }
+                      content={(bag) => (
+                        <ProjectForm
+                          services={props.services}
+                          onCancel={bag.close}
+                          mode="edit"
+                          defaultValues={project}
+                          onSubmit={async (data) => {
+                            await props.services.mutationService.editProject(
+                              project.id,
+                              data,
+                            );
+                            bag.close();
+                          }}
+                        />
+                      )}
+                    />
+                  </>
+                ))}
+            </ActionMenu>
+          </CardTitle>
+          <CardDescription>
+            {rd
+              .journey(project)
+              .wait(<Skeleton className="h-[1lh] w-30" />)
+              .catch(renderError)
+              .map((x) => x.description)}
+          </CardDescription>
+        </CardHeader>
+      </Card>
       <ListView
         data={projectIterations}
         query={query}
