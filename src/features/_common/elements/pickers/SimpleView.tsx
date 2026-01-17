53fc826d29612a00317b3828a9087a3c95814585
adamborowski <fl.borovsky@gmail.com>
2026-01-17 22:00:02 +0100

    feat: add SimpleArrayPicker and SimpleView components for item selection
    - Introduced SimpleArrayPicker component to facilitate multi-item selection with filtering capabilities.
    - Created SimpleView component to render individual items with a consistent layout.
    - Enhanced user experience with customizable placeholders and item sizes in the picker.

---
+++ src/features/_common/elements/pickers/SimpleView.tsx
@@ -0,0 +1,29 @@
+import {
+  AbstractEntityView,
+  AbstractEntityViewProps,
+} from "@/features/_common/elements/pickers/_common/AbstractEntityView.tsx";
+import { SwitchProps } from "@/platform/typescript/SwitchProps.ts";
+import { rd, RemoteData } from "@passionware/monads";
+
+export interface SimpleItem {
+  id: string;
+  label: string;
+}
+
+export type SimpleViewProps = SwitchProps<
+  AbstractEntityViewProps,
+  "entity",
+  { item: RemoteData<SimpleItem> }
+>;
+
+export function SimpleView({ item, ...props }: SimpleViewProps) {
+  return (
+    <AbstractEntityView
+      entity={rd.map(item, (x) => ({
+        name: x.label,
+        avatarUrl: null,
+      }))}
+      {...props}
+    />
+  );
+}
